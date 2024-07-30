999342b7ed8d29f04d2238445cb8a254f1307d24
Ramanuj Das <ramanujds9@gmail.com>
2024-07-31 02:50:20 +0530

    Added buildspec.yml

---
+++ src/test/java/com/rds/example/SpringBootCicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rds.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
