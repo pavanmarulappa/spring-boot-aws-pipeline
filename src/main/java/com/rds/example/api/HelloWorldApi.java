c857e3999148de1b30539f087b74a7891c03900e
Ramanuj Das <ramanujds9@gmail.com>
2024-07-31 02:49:14 +0530

    Added buildspec.yml

---
+++ src/main/java/com/rds/example/api/HelloWorldApi.java
@@ -0,0 +1,14 @@
+package com.rds.example.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldApi {
+
+    @GetMapping
+    public String helloWorld() {
+        return "Hello World!";
+    }
+
+}
